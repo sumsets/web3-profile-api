d9f357c237bcf55a683e136dd32d5248f4926bbf
ridvansumset <ridvan.sumset@coda.xyz>
2022-04-09 12:00:34 +0100

    add CORS settings

--- api/api.go
+++ api/api.go
@@ -7,8 +7,26 @@ import (
 
 func RunAPI() {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/users/:id", getUserByID, SleepMiddleware())
 	router.Run("localhost:2323")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 // delay requests manually since we don't have a real DB
--- model/user.go
+++ model/user.go
@@ -3,7 +3,7 @@ package model
 type User struct {
 	ID            string   `json:"id"`
 	Name          string   `json:"name"`
-	ProfileLink string `json:"profile_link"`
+	ProfileLink   string   `json:"profile_link"`
 	WalletAddress string   `json:"walletAddress"`
 	Tokens        int64    `json:"tokens"`
 	Badges        []string `json:"badges"`
